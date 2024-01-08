cd6945d9ebafd3b59203e3ab9c919f1b0beedac3
padhiarmeet <145247660+padhiarmeet@users.noreply.github.com>
2024-01-08 09:26:22 +0530

    Add files via upload

---
+++ htmlmeet/lab.3.a.c
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    
+    <title>Document</title>
+</head>
+<body>
+    <table border="2" cellspacing=0>
+        <tr>
+           <td colspan="4" rowspan="3">A</td>
+           <td colspan="2">D</td>
+           <td colspan="2">E</td>
+        </tr>
+        <tr>
+            <td colspan="2">F</td>
+           <td colspan="2">G</td>
+         </tr>
+         <tr>
+            <td colspan="2">h</td>
+           <td colspan="2">I</td>
+         </tr>
+         <tr>
+            <td colspan="4" rowspan="2" height="40px">b</td>
+            <td colspan="2">j</td>
+            <td colspan="2">k</td>
+         </tr>
+         <tr>
+            <td colspan="4" rowspan="2">l</td>
+           
+         </tr>
+         <tr>
+            <td colspan="4" rowspan="1" height="20px">c</td>
+           
+         </tr>
+         
+         <tr align="center" height="25px">
+            <th>SL</th>
+            <th>items</th>
+            <th>due date</th>
+            <th>qte</th>
+            <th>rate</th>
+            <th>per</th>
+            <th>dis%</th>
+            <th>amt</th>
+        </tr>
+        <tr  align="center" height="25px">
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        </tr>
+        <tr align="center" height="25px">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+    
+        <tr  align="center" height="25px">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    
+        <tr align="center" height="25px">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+    
+        <tr align="center" height="25px">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr align="center" height="25px">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr>
+            <td colspan="7"></td>
+            <td>E.&OE.</td>
+        </tr>
+    <tr align="center" height="25px">
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    </tr>
+    <tr  align="center" height="25px">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td colspan="4" rowspan="4"></td>
+       
+        </tr>
+        <tr align="center" height="25px">
+            <td colspan="3"></td>
+            <td></td>
+            
+           
+     </tr>
+     <tr align="center" height="25px">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                
+               
+    </tr>
+    <tr align="center" height="25px">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                   
+                   
+    </tr>
+        
+    </table>
+    
+</body>
+</html>
